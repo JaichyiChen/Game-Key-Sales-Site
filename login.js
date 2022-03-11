950fa6cbbc65f06b036e1f0570f8f25052754b7b
Jaichyi <84934225+JaichyiChen@users.noreply.github.com>
2022-03-11 16:18:54 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,4 @@
 //login
-const input = document.querySelector(".input");
 const labels = document.querySelectorAll(".form-control label");
 labels.forEach((label) => {
   label.innerHTML = label.innerText
